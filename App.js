1ed5da305d77fc441645f12b48a440bc5fd1ede6
SACHIN KUMAR PAL <101247442+sachin59921@users.noreply.github.com>
2024-12-26 10:39:01 +0530

    add files

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AdminDashboard from "./components/AdminDashboard";
+import Home from "./components/Home";
+import VisitorRegistration from "./components/VisitorRegistration";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  const appStyle = {
+    marginTop: "60px",
+  };
+
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+        <div style={appStyle}>
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/visitor" element={<VisitorRegistration />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
